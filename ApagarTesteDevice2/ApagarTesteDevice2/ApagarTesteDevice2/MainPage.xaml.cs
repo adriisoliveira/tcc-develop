98c0faae2abe2982e744c0a4b303a63d6bb90ce3
phlogan <pheliplogann@gmail.com>
2021-06-17 21:22:29 -0300

    TCC-20 first commit

---
+++ ApagarTesteDevice2/ApagarTesteDevice2/ApagarTesteDevice2/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Plugin.BLE;
+using Plugin.BLE.Abstractions;
+using Plugin.BLE.Abstractions.Contracts;
+using Plugin.BLE.Abstractions.Exceptions;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading;
+using Xamarin.Forms;
+
+namespace ApagarTesteDevice2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        IAdapter adapter;
+        IBluetoothLE bluetoothBLE;
+        ObservableCollection<IDevice> list;
+        IDevice device;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            bluetoothBLE = CrossBluetoothLE.Current;
+            adapter = CrossBluetoothLE.Current.Adapter;
+
+            list = new ObservableCollection<IDevice>();
+            DevicesList.ItemsSource = list;
+
+        }
+
+        private async void searchDevice(object sender, EventArgs e)
+        {
+            if (bluetoothBLE.State == BluetoothState.Off)
+            {
+                await DisplayAlert("Atenção", "Bluetooth desabilitado.", "OK");
+            }
+            else
+            {
+                list.Clear();
+
+                adapter.ScanTimeout = 10000;
+                adapter.ScanMode = ScanMode.Balanced;
+
+
+                adapter.DeviceDiscovered += (obj, a) =>
+                {
+                    if (!list.Contains(a.Device))
+                        list.Add(a.Device);
+                };
+
+                await adapter.StartScanningForDevicesAsync();
+
+            }
+
+        }
+
+        private async void DevicesList_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            device = DevicesList.SelectedItem as IDevice;
+
+            var result = await DisplayAlert("AVISO", "Deseja se conectar a esse dispositivo?", "Conectar", "Cancelar");
+
+            if (!result)
+                return;
+
+            //Stop Scanner
+            await adapter.StopScanningForDevicesAsync();
+
+            try
+            {
+                var parameters = new ConnectParameters(forceBleTransport: true);
+                //await _adapter.ConnectToDeviceAsync(deviceToConnect, parameters, cancellationToken.Token);
+
+                await adapter.ConnectToDeviceAsync(device, parameters, new CancellationTokenSource().Token);
+
+                await DisplayAlert("Conectado", "Status:" + device.State, "OK");
+                await adapter.DisconnectDeviceAsync(device);
+
+            }
+            catch (DeviceConnectionException ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+
+        }
+    }
+}
