d35baa38c048596c3521e0d6510034e6ac46c630
adriisoliveira <adrianne.soliveira@hotmail.com>
2021-09-21 21:01:06 -0300

    TCC-27 Refactor 2

--- IA Busca em Texto/src/database/Script Count.sql
+++
@@ -1,8 +0,0 @@
-select count(*) from urls where idurl in (select idurl from palavra_localizacao);
-select count(*) from palavra_localizacao;
-select count(*) from url_ligacao;
-select count(*) from url_palavra;
-
-select * from url_ligacao;
-select * from urls order by idurl;
-select count(*) from url_ligacao where idurl_destino = 2826;
