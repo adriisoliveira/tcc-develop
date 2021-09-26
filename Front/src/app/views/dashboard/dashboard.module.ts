d79c1a4c4fbc77de9cb925fc7573f1caded9fee8
alqueiroz <alan.queiroz@capgemini.com>
2021-09-26 16:57:21 -0300

    TCC-31 Create a CallBack program to test return from dashboard.component.html

--- Front/src/app/views/dashboard/dashboard.module.ts
+++ Front/src/app/views/dashboard/dashboard.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 
+import {MenubarModule} from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';//item q vai no componet
+
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    MenubarModule, 
   ]
 })
 export class DashboardModule { }
