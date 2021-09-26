ee8764091a2d64d71882bead73e4b2c9c4e6b747
alqueiroz <alan.queiroz@capgemini.com>
2021-09-26 15:00:37 -0300

    TCC-31 Refactory resumo-texto.component

--- Front/src/app/views/resumo-texto/resumo-texto.component.ts
+++ Front/src/app/views/resumo-texto/resumo-texto.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-resumo-texto',
   templateUrl: './resumo-texto.component.html',
--- Front/src/app/views/resumo-texto/resumo-texto.module.ts
+++ Front/src/app/views/resumo-texto/resumo-texto.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ResumoTextoRoutingModule } from './resumo-texto-routing.module';
 import { ResumoTextoComponent } from './resumo-texto.component';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+
 
 
 @NgModule({
   declarations: [ResumoTextoComponent],
   imports: [
     CommonModule,
-    ResumoTextoRoutingModule
+    ResumoTextoRoutingModule,
+    InputTextareaModule,
   ]
 })
 export class ResumoTextoModule { }
