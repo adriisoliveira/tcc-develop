077da5f392d8ea3b60f586118fe4d337168ffb5b
alqueiroz <alan.queiroz@capgemini.com>
2021-09-30 23:37:52 -0300

    TCC-31 Move the table logic to rank-page-rank.component.html

--- Front/src/app/views/page-rank/page-rank.component.ts
+++ Front/src/app/views/page-rank/page-rank.component.ts
@@ -13,6 +13,14 @@ export class PageRankComponent implements OnInit {
 
   books: Book[];
 
+  //to use a Dynamic table inpute any coluns, coluns do html
+  cols: any[];
+
+  totalRecords: number;
+
+  //to use for a costum filter like used Author to express
+  allAuthors: any[];
+
   constructor(
     private router: Router,
     private bookService: BookService
@@ -21,10 +29,34 @@ export class PageRankComponent implements OnInit {
   ngOnInit()  {
     this.bookService.getBooks().
       then(books => this.books = books);
+
+    /*Create a backing object named cols. It will be an array of name-value pairs for the columnn header names. */
+    //to inicialize the filter from colun by field
+    this.cols = [
+      { field: 'name', header: 'Name' },
+      {field: 'author', header: 'Author' },
+      { field: 'price', header: 'Price' }      
+    ];
+
+    //no momento error no console browser
+    //core.js:6241 ERROR TypeError: Cannot read properties of undefined (reading 'length')
+    this.totalRecords=this.books.length;
+
+    //used tu imput a costum filtler inside a colun for HTML
+    this.allAuthors = [
+      { label: 'All Authors', value: null },
+      { label: 'Mario Puzo', value: 'Mario Puzo' },
+      { label: 'J.R.R. Tolkien', value: 'J.R.R. Tolkien' },
+      { label: 'J.K. Rowling', value: 'J.K. Rowling' },
+      { label: 'author1', value: 'author1' },
+      { label: 'author2', value: 'author2' },
+      { label: 'author3', value: 'author3' },   
+    ];
+
   }
 
   public doBackToMenu(): void{
-    this.router.navigate(['dashboard'])
+      this.router.navigate(['dashboard'])
   }
 
 }
