d81c05ae049b3f2af01616b6f6a6567c6fee05a7
phlogan <pheliplogann@gmail.com>
2021-10-07 21:53:36 -0300

    l

---
+++ WebCrawler/WebCrawler.API/Controllers/TesteController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebCrawler.API.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Mvc.Route("[controller]")]
+    public class TesteController : ControllerBase
+    {
+        public string Teste()
+        {
+            return "HELLO WORLD!";
+        }
+    }
+}
