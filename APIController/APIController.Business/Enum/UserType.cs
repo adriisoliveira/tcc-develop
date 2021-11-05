9bedbb1fbfa6242e8f58f098962d86e171bd4a8b
adriisoliveira <adrianne.soliveira@hotmail.com>
2021-11-04 23:31:36 -0300

    Revert "Merge branch 'main' into develop"
    This reverts commit 3fc4f8118de56a1de58a6cb04b6f86f622da8c12, reversing
    changes made to d70ddbb4a37abd8cdbde928b8166536a5e7c30d2.

---
+++ APIController/APIController.Business/Enum/UserType.cs
@@ -0,0 +1,10 @@
+﻿namespace APIController.Business.Enum
+{
+    public enum UserType : int
+    {
+        Unknown = 0,
+        Teacher = 1,
+        Student = 2,
+        InstitutionAdministrator = 4
+    }
+}
