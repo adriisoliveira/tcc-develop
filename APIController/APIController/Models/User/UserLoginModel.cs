b7492855ffd5087bea96cef53cd234f19d5bb323
Adri Oliveira <adrianne.oliveira@capgemini.com>
2021-11-10 21:21:15 -0300

    TCC-57 ajust branch

--- APIController/APIController/Models/User/UserLoginModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace APIController.Models.User
-{
-    public class UserLoginModel
-    {
-        public string Login { get; set; }
-        public string Password { get; set; }
-    }
-}
--- Front/src/app/resources/models/RequestLogin.ts
+++
@@ -1,4 +0,0 @@
-export class RequestLogin {
-  public login: string;
-  public password: string;
-}
