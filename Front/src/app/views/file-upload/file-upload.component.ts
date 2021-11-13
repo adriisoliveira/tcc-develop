2a06966444f3ac9180f19910c818c2049c3d4e3d
phlogan <pheliplogann@gmail.com>
2021-11-12 23:00:05 -0300

    melhorias no envio de arquivo

--- Front/src/app/views/file-upload/file-upload.component.ts
+++ Front/src/app/views/file-upload/file-upload.component.ts
@@ -3,9 +3,9 @@ import { Router } from '@angular/router';
 
 import {MenuItem} from 'primeng/api';
 import {AlertService} from '../../resources/services/alert.service';
-import { FileUploadService } from '../../resources/services/fileupload.service';
+// import { FileUploadService } from '../../resources/services/fileupload.service';
 import {FileUploadModule} from 'primeng/fileupload';
-import {HttpClient, HttpEventType, HttpRequest, HttpClientModule} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpEventType, HttpRequest, HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-file-upload',
@@ -18,48 +18,58 @@ export class FileUploadComponent implements OnInit {
   fileName: String;
   items: MenuItem[];
   alertService: AlertService;
+  author: String;
 
   constructor(
     private router: Router,
-    private fileUploadService: FileUploadService,
+    // private fileUploadService: FileUploadService,
     private http: HttpClient
   ) {
     this.fileName = "";
-    this.fileUploadService = new FileUploadService(http);
+    // this.fileUploadService = new FileUploadService(http);
     this.alertService = new AlertService();
+    this.author = "";
   }
 
   onFileSelected(event) {
     this.fileName = event.target.files[0].name;
   }
 
-    upload(files) {  
-      if (files.length === 0){
-        console.log('nada');
-        return;
-      }
-    
-      const formData = new FormData();  
-    
-      for (let file of files)
-        formData.append(file.name, file);  
-    
-      const uploadReq = new HttpRequest('POST', 'https://localhost:44312/file/save', formData, {  
-        reportProgress: true,
-      }); 
-    
-      this.http.request(uploadReq).subscribe(event => { 
-        if (event.type === HttpEventType.Response)
-        {
-          if(event.status == 200){
-            this.alertService.info('Sucesso', 'Arquivo enviado com sucesso');
-            this.fileName = "";
-          }else{
-            this.alertService.error('Erro', 'O arquivo não pôde ser enviado');
-          }
-        }
-      });
+  upload(files) {  
+    if (files.length === 0){
+      return;
     }
+    
+    const formData = new FormData();  
+    
+    for (let file of files){
+      formData.append(file.name, file);  
+    }
+    
+    formData.append("author", this.author.toString());
+
+    let headers = new HttpHeaders({
+      'Authorization' : 'bearer '+ localStorage.getItem('loginResponseJwt')
+    });
+    
+    const uploadReq = new HttpRequest('POST', 'https://localhost:44312/file/save', formData, {
+      headers: headers,
+      reportProgress: true,
+    });
+  
+    this.http.request(uploadReq).subscribe(event => {
+      if (event.type === HttpEventType.Response)
+      {
+        if(event.status == 200){
+          this.alertService.info('Sucesso', 'Arquivo enviado com sucesso');
+          this.fileName = "";
+          this.author = "";
+        }else{
+          this.alertService.error('Erro', 'O arquivo não pôde ser enviado');
+        }
+      }
+    });
+  }
 
   ngOnInit()  {
     this.items = [
