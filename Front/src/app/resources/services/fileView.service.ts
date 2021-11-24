9f707822bfdb254d9343c3cc703e64c5458f841f
alqueiroz <alan.queiroz@capgemini.com>
2021-11-23 21:11:37 -0300

    TCC-61 Get Johny updates

--- Front/src/app/resources/services/fileView.service.ts
+++ Front/src/app/resources/services/fileView.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders, HttpEventType, HttpRequest, HttpClientModule} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {FileView } from '../models/FileView';
 import { Observable } from 'rxjs';
 
@@ -8,35 +8,15 @@ import { Observable } from 'rxjs';
 })
 
 export class FileViewService {
+  files: FileView[];
+  constructor(private http: HttpClient) {}
 
-    files: FileView[];
-    constructor(private http: HttpClient) {}
-
-    render( ):Observable<FileView[]> {
-
-        // const httpOptions = {
-        //     headers: new HttpHeaders({
-        //       'Authorization' : 'bearer '+ localStorage.getItem('loginResponseJwt')
-        //     })
-        //   }
-
-        // return this.http.get<any>(`https://localhost:44312/file/getAll/ecologia?maxResults=25`,httpOptions)
-        // .toPromise()
-        // .then(res => <FileView[]>res.json())
-        // .then(data => { return data; });
-        
-      
-        // return this.http.get<FileView[]>(`https://localhost:44312/file/getAll/ecologia?maxResults=25`, httpOptions);
-
-
-        const httpOptions = {
-            headers: new HttpHeaders({
-              'Authorization' : 'bearer '+ localStorage.getItem('loginResponseJwt')
-            })
-          }
-          return this.http.get<any>(`https://localhost:44312/file/getAll/estigmas?maxResults=25`, httpOptions);
-          // this.http.get<IFileView[]>(`https://localhost:44312/file/getAll/estigmas?maxResults=25`, httpOptions).subscribe();
-
-        }
-      
+  render():Observable<FileView[]> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization' : 'bearer '+ localStorage.getItem('loginResponseJwt')
+      })
     }
+    return this.http.get<any>(`https://localhost:44312/file/topRecent/25`, httpOptions);
+  }
+}
